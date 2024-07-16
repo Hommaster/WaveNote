1c507ae225a78c8f3eff16b5b108e581edbc67f1
Hommaster <ilya.pan.2017@mail.ru>
2024-07-16 12:56:34 +0300

    added compose navigation, in NavHost added function MAinScreen.kt and custom topAppBar CustomTopAppBar.kt

--- app/src/main/java/com/example/wavenote/MainActivity.kt
+++ app/src/main/java/com/example/wavenote/MainActivity.kt
@@ -5,30 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.wavenote.helpers.CustomAppBar
+import com.example.wavenote.screens.mainScreen.MainScreen
 import com.example.wavenote.ui.theme.WaveNoteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
+            val navController = rememberNavController()
             WaveNoteTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    content = { paddingValues ->
-                        Text(
-                            modifier = Modifier
-                                .padding(paddingValues),
-                            text = "Hellow word!"
+                NavHost(navController = navController, startDestination = "main_screen") {
+                    composable("main_screen") {
+                        Scaffold(
+                            modifier = Modifier.fillMaxSize(),
+                            topBar = {
+                                CustomAppBar()
+                            },
+                            content = { paddingValues ->
+                                MainScreen(paddingValues = paddingValues)
+                            }
                         )
                     }
-                )
+                }
             }
         }
     }
