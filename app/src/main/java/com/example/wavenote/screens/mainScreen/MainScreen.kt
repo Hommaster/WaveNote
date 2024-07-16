899ac88a92c71688394998174cd269b0e2c1ac09
Hommaster <ilya.pan.2017@mail.ru>
2024-07-16 12:55:07 +0300

    created composable function MainScreen for main screen app

--- app/src/main/java/com/example/wavenote/screens/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/wavenote/screens/mainScreen/MainScreen.kt
@@ -1,2 +1,18 @@
 package com.example.wavenote.screens.mainScreen
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(
+    paddingValues: PaddingValues
+) {
+    Text(
+        modifier = Modifier
+            .padding(paddingValues),
+        text = "Hello word!"
+    )
+}
