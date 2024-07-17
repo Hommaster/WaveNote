c14c2a2e2637a6d0e1b8e0a81366c1d017f276ff
Hommaster <ilya.pan.2017@mail.ru>
2024-07-17 23:30:32 +0300

    added new composable component for NavHost with file CalendarApp.kt

--- app/src/main/java/com/example/wavenote/MainActivity.kt
+++ app/src/main/java/com/example/wavenote/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.wavenote
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.wavenote.helpers.CustomAppBar
+import com.example.wavenote.routes.Routes
+import com.example.wavenote.screens.calendar.CalendarApp
 import com.example.wavenote.screens.mainScreen.MainScreen
 import com.example.wavenote.ui.theme.WaveNoteTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,17 +26,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             WaveNoteTheme {
-                NavHost(navController = navController, startDestination = "main_screen") {
-                    composable("main_screen") {
+                NavHost(navController = navController, startDestination = Routes.Home.route) {
+                    composable(Routes.Home.route) {
                         Scaffold(
-                            modifier = Modifier.fillMaxSize(),
-                            topBar = {
-                                CustomAppBar()
-                            },
+                            modifier = Modifier
+                                .fillMaxSize(),
                             content = { paddingValues ->
-                                MainScreen(paddingValues = paddingValues)
+                                MainScreen(
+                                    navController = navController,
+                                    paddingValues = paddingValues
+                                )
                             }
                         )
+                    }
+                    composable(Routes.Calendar.route){
+                        CalendarApp()
                     }
                 }
             }
