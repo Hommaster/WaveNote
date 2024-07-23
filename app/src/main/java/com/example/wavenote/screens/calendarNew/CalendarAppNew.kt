aced1334125fe017e506aac087fa9dd30022aa70
Hommaster <ilya.pan.2017@mail.ru>
2024-07-23 23:27:24 +0300

    added functional for checking day-state with click

--- app/src/main/java/com/example/wavenote/screens/calendarNew/CalendarAppNew.kt
+++ app/src/main/java/com/example/wavenote/screens/calendarNew/CalendarAppNew.kt
@@ -2,36 +2,61 @@ package com.example.wavenote.screens.calendarNew
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.wavenote.screens.calendar.util.getDisplayName
+import com.example.wavenote.R
+import com.example.wavenote.helpers.PulsarFab
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
 import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
 import com.kizitonwose.calendar.core.daysOfWeek
-import shark.PrimitiveType
 import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.Month
 import java.time.YearMonth
 import java.time.format.TextStyle
 import java.util.Locale
 
 @RequiresApi(Build.VERSION_CODES.O)
+@Preview
 @Composable
-fun CalendarAppNew(
+fun PreviewCalendarNewApp(){
+    CalendarApp()
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun CalendarApp(
 
 ) {
     val currentMonth = remember { YearMonth.now() }
@@ -47,33 +72,79 @@ fun CalendarAppNew(
         firstDayOfWeek = daysOfWeek.first()
     )
 
-    Column {
+    var selectedDate by remember { mutableStateOf<LocalDate?>(LocalDate.now()) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.very_light_brown))
+    ) {
         HorizontalCalendar(
             state = state,
-            dayContent = { Day(it) },
+            dayContent = { day ->
+                Day(day, isSelected = selectedDate == day.date) { calendarDay ->
+                    selectedDate = if (selectedDate == calendarDay.date) null else calendarDay.date
+                }
+            },
             monthHeader = { month ->
-                val daysOfWeek = month.weekDays.first().map { it.date.dayOfWeek }
-                val monthOfYear = month.yearMonth.month
-                val year = month.yearMonth.year
-                MonthAndYearTitle(
-                    monthOfYear,
-                    year
+                TopAppBar(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(0.dp, 0.dp, 13.dp, 13.dp))
+                        .background(
+                            color = Color.Gray,
+                            RoundedCornerShape(0.dp, 13.dp, 13.dp, 0.dp)
+                        ),
+                    colors = TopAppBarDefaults.topAppBarColors(colorResource(id = R.color.brown_light)),
+                    title = {
+                        val monthOfYear = month.yearMonth.month
+                        val year = month.yearMonth.year
+                        MonthAndYearTitle(
+                            monthOfYear,
+                            year
+                        )
+                    }
                 )
                 DaysOfWeekTitle(daysOfWeek = daysOfWeek)
-            }
+            },
         )
+        Box(
+            modifier = Modifier
+                .padding(268.dp, 297.dp, 0.dp, 0.dp),
+        ) {
+            PulsarFab(
+                onClick = {
+
+                }
+            )
+        }
     }
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun Day(day: CalendarDay) {
+fun Day(
+    day: CalendarDay,
+    isSelected: Boolean,
+    onClick: (CalendarDay) -> Unit
+    ) {
     Box(
         modifier = Modifier
-            .aspectRatio(1f), // This is important for square sizing!
+            .aspectRatio(1f)
+            .clip(CircleShape)
+            .background(color = if (isSelected) colorResource(id = R.color.orange) else Color.Transparent)
+            .clickable(
+                enabled = day.position == DayPosition.MonthDate,
+                onClick = {
+                    onClick(day)
+                }
+            ),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = day.date.dayOfMonth.toString())
+        Text(
+            text = day.date.dayOfMonth.toString(),
+            color = if(day.position == DayPosition.MonthDate) Color.White else Color.Gray
+        )
     }
 }
 
