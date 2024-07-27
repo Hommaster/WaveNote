34c3065d97d571a6ddcc19ad5d33e1c7edb138eb
Hommaster <ilya.pan.2017@mail.ru>
2024-07-27 23:24:35 +0300

    created dao interface for NoteData.kt for query request to Room-database

---
+++ app/src/main/java/com/example/wavenote/database/note/NoteDataDao.kt
@@ -0,0 +1,41 @@
+package com.example.wavenote.database.note
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+@Dao
+interface NoteDataDao {
+
+    @Query("SELECT * FROM notedata")
+    fun getAllNotes(): Flow<List<NoteData>>
+
+    @Query("DELETE FROM notedata")
+    suspend fun deleteAll()
+
+    @Query("DELETE FRom notedata WHERE calendarDay=(:calendarDay)")
+    suspend fun deleteAllFromLocalDate(calendarDay: LocalDate)
+
+    @Query("DELETE FRom notedata WHERE category=(:category)")
+    suspend fun deleteAllFromCategory(category: String)
+
+    @Query("SELECT * FROM notedata WHERE calendarDay=(:calendarDay)")
+    suspend fun getAllNotesFromACertainLocalDate(calendarDay: LocalDate): Flow<List<NoteData>>
+
+    @Query("SELECT * FROM notedata WHERE category=(:category)")
+    suspend fun getAllNotesFromACertainCategory(category: String): Flow<List<NoteData>>
+
+    @Update
+    suspend fun updateNoteData(note: NoteData)
+
+    @Insert
+    suspend fun addNote(note: NoteData)
+
+    @Delete
+    suspend fun deleteNote(note: NoteData)
+
+}
