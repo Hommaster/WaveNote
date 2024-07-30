4a150a3164d69b69fb294112ba1bd586c8b67788
Hommaster <ilya.pan.2017@mail.ru>
2024-07-30 23:52:56 +0300

    changed theme for background

--- app/src/main/java/com/example/wavenote/ui/theme/Theme.kt
+++ app/src/main/java/com/example/wavenote/ui/theme/Theme.kt
@@ -13,19 +13,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color(0xFFBA9D83),
+    primary = Color(0xFFD68842),
     secondary = PurpleGrey80,
     tertiary = Pink80,
-    background = Color(0xFFBA9D83),
-    onBackground = Color(0xFFBA9D83),
+    background = Color(0xFFD68842),
+    onBackground = Color(0xFFD68842),
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40,
-    background = Color(0xFFBA9D83),
-    onBackground = Color(0xFFBA9D83),
+    background = Color(0xFFD68842),
+    onBackground = Color(0xFFD68842),
 
     /* Other default colors to override
     surface = Color(0xFFFFFBFE),
