9f0a74b00384ef256a901fb8ca29a2282d50e25a
Hommaster <ilya.pan.2017@mail.ru>
2024-08-05 01:42:36 +0300

    created new argument for data class and created NavType for this class

--- app/src/main/java/com/example/wavenote/database/note/NoteData.kt
+++ app/src/main/java/com/example/wavenote/database/note/NoteData.kt
@@ -1,9 +1,14 @@
 package com.example.wavenote.database.note
 
+import android.os.Build
+import android.os.Bundle
 import android.os.Parcelable
+import androidx.annotation.RequiresApi
+import androidx.navigation.NavType
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.wavenote.database.categories.Categories
+import com.google.gson.Gson
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 import java.util.Date
@@ -19,5 +24,22 @@ data class NoteData(
     val description: String = "",
     val fileNameAudio: String = "",
     val fileNameImage: String = "",
-    val category: String = Categories().nameCategory
+    val category: String = Categories().nameCategory,
+    val typeNote: String = "",
 ) : Parcelable
+
+
+class NoteDataNavType : NavType<NoteData>(isNullableAllowed = true) {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun get(bundle: Bundle, key: String): NoteData? {
+        return bundle.getParcelable(key, NoteData::class.java)
+    }
+
+    override fun parseValue(value: String): NoteData {
+        return Gson().fromJson(value, NoteData::class.java)
+    }
+
+    override fun put(bundle: Bundle, key: String, value: NoteData) {
+        bundle.putParcelable(key, value)
+    }
+}
